ddce54227289d33e4a3e2ec8b2e193175b1e2bd7
ence1ad <romanrusl@mail.ru>
2023-07-22 19:31:46 +0300

    Create json_io/json_io.py for save and get json

---
+++ json_io/json_io.py
@@ -0,0 +1,19 @@
+import json
+
+from router import path_to_json
+
+
+def save_json(filename: str, data: dict) -> None:
+    title = filename + '.json'
+    file = f'{path_to_json}/{title}'
+    with open(file=file, mode='w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+def get_json(filename: str) -> json:
+    title = filename + '.json'
+    file = f'{path_to_json}/{title}'
+    with open(file=file, mode='r', encoding='utf-8') as f:
+        data = json.load(f)
+        return data
+
